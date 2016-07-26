d5589f82edc21e7b7d5cb33ededbbd58b2aa1921
Aleksandr Chekalin <chekalin87@gmail.com>
2016-07-26 17:49:52 +0300

    Первая заготовка струйки

---
+++ matrixRain.lua
@@ -0,0 +1,26 @@
+local component = require("component")
+local gpu = component.gpu
+local unicode = require("unicode")
+--local thread = require("thread")
+local w, h = gpu.getResolution()
+
+local char = {12040, 12065, 12090, 12103, 12144, 12043, 12047, 7112, 48, 81, 56} 
+local length = #char
+
+local function drop(x)
+    local t = {} 
+    for i=1, w + 10 do
+        local v = i
+        table.insert(t, 1, unicode.char(char[math.random(length)]))
+        for j=1, 10 do
+            if v >= 1 and v < w then
+              gpu.set(x, v, t[j])
+            end
+            v = v - 1
+        end
+        gpu.set(x, v, " ")
+        os.sleep(0.1)
+    end
+end
+
+drop(5)
