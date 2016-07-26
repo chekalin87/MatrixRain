9650a3e69e8517519901fb94a6e8868480407b30
Aleksandr Chekalin <chekalin87@gmail.com>
2016-07-27 01:42:40 +0300

    version 0.1.3
    Дополнения, улучшения комментарии.

--- matrixRain.lua
+++ matrixRain.lua
@@ -1,10 +1,13 @@
+-- Спасибо Zer0Galaxy за библиотеку Thread.
+-- pastebin get E0SzJcCx /lib/thread.lua
+
 local component = require("component")
 local gpu = component.gpu
 local unicode = require("unicode")
 local thread = require("thread")
 local w, h = gpu.getResolution()
 
-
+-- символы юникод
 local char = {12040, 12065, 12090, 12103, 12144, 12043, 12047, 7112, 48, 81, 56, 2059, 2069, 3908, 3946, 3947, 7249, 7193, 7934, 8040, 5862, 5809, 5871, 5801, 12452, 12456, 12640, 12530, 12484, 12793, 12796, 12799, 12784}
 local lengthChar = #char
 
@@ -12,11 +15,11 @@ local function drop(x)
     local t = {} 
     while true do
         local speed = math.random()
-        while speed < 0.2 do
+        while speed < 0.05 do                                                     -- ограничение разницы скоростей капель
             speed = math.random()
         end
-        local length = math.random(7, math.floor(h*1.5))
-        os.sleep(math.random(0, 10))
+        local length = math.random(5, math.floor(h + 0.2*h))                     -- длина капли
+        os.sleep(math.random(0, 7))                                             -- частота капель
         for i=1, h + length do
             local v = i
             table.insert(t, 1, unicode.char(char[math.random(lengthChar)]))
@@ -25,13 +28,13 @@ local function drop(x)
                     if j == 1 then
                         gpu.setForeground(0xFFFFFF)
                         gpu.set(x, v, t[j])
-                    elseif j == math.floor(length - length/3) then
+                    elseif j == math.floor(length - length / 3) then
                         gpu.setForeground(0x006000)
                         gpu.set(x, v, t[j]) 
-                    elseif j == math.floor(length - length/6)  then
+                    elseif j == math.floor(length - length / 6) then
                         gpu.setForeground(0x004000)
                         gpu.set(x, v, t[j])
-                    elseif j == math.floor(length - length/12)  then
+                    elseif j == math.floor(length - length /12) then
                         gpu.setForeground(0x002000)
                         gpu.set(x, v, t[j])
                     elseif j == 2 then
@@ -42,7 +45,7 @@ local function drop(x)
                 v = v - 1
             end
             gpu.set(x, v, " ")
-            os.sleep(speed/8)
+            os.sleep(speed/8)                                                    --скорость капель
         end
     end
 end
@@ -52,5 +55,6 @@ gpu.setForeground(0x00FF00)
 thread.init()
 for i=1, w, 2 do
     thread.create(drop,i)
+    thread.create(drop,i)                                                      --если раскомментировать - будет больше похоже на оригинал(кому как нравится)  
 end
 thread.waitForAll()
